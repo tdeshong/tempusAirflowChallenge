802280bd57faec535bc9683d4c5246dca6d8701e
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-05-30 18:07:52 -0400

    Update topHighlight_dag.py

--- dags/topHighlight_dag.py
+++ dags/topHighlight_dag.py
@@ -24,7 +24,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 )
 
-dag = DAG('tempus', default_args=default_args,
+dag = DAG(dag_id, default_args=default_args,
            description = 'Newsapi top highlights',
            schedule_interval = timedelta(days=1),
 )
