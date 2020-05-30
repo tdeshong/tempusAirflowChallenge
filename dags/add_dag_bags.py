e8e4c18d3c3b13a48b87cbacb69a10136e53fcee
tdeshong <36869042+tdeshong@users.noreply.github.com>
2020-05-30 17:05:03 -0400

    Create add_dag_bags.py

---
+++ dags/add_dag_bags.py
@@ -0,0 +1,10 @@
+import os
+from airflow.models import DagBag
+dags_dirs = ['../anyTopic']
+
+for dir in dags_dirs:
+   dag_bag = DagBag(os.path.expanduser(dir))
+
+   if dag_bag:
+      for dag_id, dag in dag_bag.dags.items():
+         globals()[dag_id] = dag
